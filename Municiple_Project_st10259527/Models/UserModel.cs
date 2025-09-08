d87473af08810cca53538cbbe90b047662ae3685
Kayla <Kaylaf3001@gmail.com>
2025-09-08 17:19:18 +0200

    Add required properties to UserModel class
    Updated `UserModel.cs` to include `FirstName`, `LastName`, `Email`, and `Password` properties, all marked as required. The `Email` property has an `[EmailAddress]` attribute for validation, and the `Password` property is designated as a password field with a `[DataType(DataType.Password)]` attribute. The `IsAdmin` property is initialized to `false` by default.

--- Municiple_Project_st10259527/Models/UserModel.cs
+++ Municiple_Project_st10259527/Models/UserModel.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace Municiple_Project_st10259527.Models
@@ -7,7 +7,21 @@ namespace Municiple_Project_st10259527.Models
     {
         [Key]
         public int UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        public bool IsAdmin { get; set; } = false;
     }
 }
